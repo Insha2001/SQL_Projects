f48deff8b61b643f6005a1fbd061c6e7328f807b
Insha2001 <81051397+Insha2001@users.noreply.github.com>
2023-08-18 17:18:33 +0530

    Update Hackerrank_problem.sql

--- Hackerrank_problem.sql
+++ Hackerrank_problem.sql
@@ -27,4 +27,26 @@ select distinct(city) from station
 where left(city,1) not in ('a','e','i','o','u') and right(city,1) not in ('a','e','i','o','u');
 
 -- Advanced Select
+-- Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
 
+-- Equilateral: It's a triangle with  sides of equal length.
+-- Isosceles: It's a triangle with  sides of equal length.
+-- Scalene: It's a triangle with  sides of differing lengths.
+-- Not A Triangle: The given values of A, B, and C don't form a triangle
+SELECT CASE 
+WHEN A+B<=C OR A+C<=B OR B+C<=A THEN "Not A Triangle"
+WHEN A=B AND A<>C THEN "Isosceles"
+WHEN A=C AND A<>B THEN "Isosceles"
+WHEN A<>B AND A<>C THEN "Scalene"
+ELSE "Equilateral"
+END FROM TRIANGLES;
+
+
+-- Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+-- Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
+select concat(name,'(',LEFT(occupation,1),')') as modify_name 
+from occupations 
+union 
+select concat('There are a total of ',count(*),' ', lower(occupation),'s.') as occupation_count 
+from occupations 
+group by occupation order by modify_name
